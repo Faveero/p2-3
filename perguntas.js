0cdc139d7d543a8e2affd622b747c2e79c603a81
matheuslindo7 <161042456+matheuslindo7@users.noreply.github.com>
2024-09-13 08:40:06 -0300

    aaaaa

--- perguntas.js
+++ perguntas.js
@@ -17,7 +17,11 @@ criaCartao(
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Bodybuilder',
+    'Oque é Mr.Olympia?',
+    'O Mr. Olympia é a competição de fisiculturismo mais prestigiada do mundo, criada em 1965 por Joe Weider, um dos grandes pioneiros do esporte. A competição se realiza anualmente e é a culminação do trabalho árduo e da dedicação extrema dos fisiculturistas. Ela é conhecida por reunir os atletas mais bem preparados e em forma, que passaram meses ou até anos treinando e ajustando suas dietas para estar em sua melhor forma no palco.
+
+Os participantes competem em várias categorias, sendo a principal a de "Open Bodybuilding", onde os competidores mostram seu desenvolvimento muscular, simetria e definição. Além dessa, existem categorias para diferentes faixas etárias e tipos de corpo, como Classic Physique, Men's Physique e Women's Bikini. O evento é transmitido ao vivo e atraí uma legião de fãs do esporte, além de ser um marco para as empresas de suplementos e roupas esportivas.
+
+O Mr. Olympia também tem um impacto significativo na cultura fitness, servindo como uma plataforma para atletas se destacarem e para novas tendências no fisiculturismo ganharem destaque. Se você curte musculação e quer ver o que é possível alcançar com muita dedicação, o Mr. Olympia é definitivamente o evento ultimate para acompanhar. É uma celebração do potencial humano e um espetáculo de força, disciplina e estética!'
 )
