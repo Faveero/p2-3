94e4d6537294e5ae1e22b7094ce9afe661a4d727
matheuslindo7 <161042456+matheuslindo7@users.noreply.github.com>
2024-09-13 08:34:43 -0300

    xxxx

--- perguntas.js
+++ perguntas.js
@@ -5,9 +5,9 @@ criaCartao(
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Bodybuilder',
+    'Atual Campeão',
+    'Chris bumstead '
 )
 
 criaCartao(
