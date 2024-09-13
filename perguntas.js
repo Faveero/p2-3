5a1971541e06590ddfe505501d89b8ed631b3d44
matheuslindo7 <161042456+matheuslindo7@users.noreply.github.com>
2024-09-13 08:44:18 -0300

    qqqqqq

--- perguntas.js
+++ perguntas.js
@@ -19,9 +19,5 @@ criaCartao(
 criaCartao(
     'Bodybuilder',
     'Oque é Mr.Olympia?',
-    'O Mr. Olympia é a competição de fisiculturismo mais prestigiada do mundo, criada em 1965 por Joe Weider, um dos grandes pioneiros do esporte. A competição se realiza anualmente e é a culminação do trabalho árduo e da dedicação extrema dos fisiculturistas. Ela é conhecida por reunir os atletas mais bem preparados e em forma, que passaram meses ou até anos treinando e ajustando suas dietas para estar em sua melhor forma no palco.
-
-Os participantes competem em várias categorias, sendo a principal a de "Open Bodybuilding", onde os competidores mostram seu desenvolvimento muscular, simetria e definição. Além dessa, existem categorias para diferentes faixas etárias e tipos de corpo, como Classic Physique, Men's Physique e Women's Bikini. O evento é transmitido ao vivo e atraí uma legião de fãs do esporte, além de ser um marco para as empresas de suplementos e roupas esportivas.
-
-O Mr. Olympia também tem um impacto significativo na cultura fitness, servindo como uma plataforma para atletas se destacarem e para novas tendências no fisiculturismo ganharem destaque. Se você curte musculação e quer ver o que é possível alcançar com muita dedicação, o Mr. Olympia é definitivamente o evento ultimate para acompanhar. É uma celebração do potencial humano e um espetáculo de força, disciplina e estética!'
+    'A Open Bodybuilding é a mais prestigiosa, mostrando o máximo desenvolvimento muscular, simetria e definição, exigindo um físico imponente e bem equilibrado. A Classic Physique remete aos campeões das décadas de 60 e 70, como Arnold Schwarzenegger, com foco em uma estética clássica e proporcional. Na Men's Physique, os atletas exibem um físico mais atlético e menos volumoso, com ênfase na definição e no estilo geral. Por fim, a Women's Bikini celebra uma aparência feminina tonificada e equilibrada, destacando uma figura esculpida sem o volume extremo das outras categorias. Cada uma dessas categorias oferece uma perspectiva única sobre o fisiculturismo, destacando diferentes padrões de força e estética.'
 )
